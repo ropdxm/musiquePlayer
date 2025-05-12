946cbfabeac739ace733a58507f9eec6f4eef0d3
Dulat <ropdxm@gmail.com>
2025-05-12 16:35:05 +0500

    capuchino balerino

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com','images.unsplash.com'],
-      },
-};
+const nextConfig = {};
 
 export default nextConfig;
