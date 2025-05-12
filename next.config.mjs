bfa9938ece19ae02d20de3c81833d152ef5fd554
Dulat <ropdxm@gmail.com>
2025-05-12 16:25:40 +0500

    lalsllsalsla

--- next.config.mjs
+++ next.config.mjs
@@ -10,23 +10,13 @@ async headers() {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "https://nfacserver.vercel.app/" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
             }
         ]
-    },
-        async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'nfacserver.vercel.app/:path*',
-          },
-        ]
-      },
-
-
+    }
 };
 
 export default nextConfig;
